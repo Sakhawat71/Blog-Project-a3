95bee18353df700b941807bd39fcdb6c0f492550
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2024-12-22 20:20:01 +0600

    delete any blog by admin

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,15 +1,21 @@
+import { BlogModel } from "../Blog/blog.model";
 import { UserModel } from "../User/user.model";
 
 // block user 
-const blockUserByAdmin = async (id : string) => {
+const blockUserByAdmin = async (id: string) => {
     return await UserModel.findByIdAndUpdate(
         id,
-        {isBlocked : true}
-    ); 
+        { isBlocked: true }
+    );
 };
 
 
+//delete blog
+const deleteBlogByAdmin = async (id: string) => {
+    return await BlogModel.findByIdAndDelete(id);
+};
+
 export const adminServices = {
     blockUserByAdmin,
-
+    deleteBlogByAdmin,
 }
